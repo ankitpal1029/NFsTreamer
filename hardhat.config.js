2be949362c3ae0f72cf73fda1ce4d5f2f05da693
ankitpal1029 <ankitkumarpal27@gmail.com>
2021-10-24 16:27:12 +0530

    intial commit

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomiclabs/hardhat-waffle");
+const { PROJECT_ID, PRIVATE_KEY } = process.env;
+
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${PROJECT_ID}`,
+      accounts: [PRIVATE_KEY],
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${PROJECT_ID}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  solidity: "0.8.4",
+};
