80787ccc74c8da9bad64d4a9c32e85e6747997ca
ankitpal1029 <ankitkumarpal27@gmail.com>
2021-10-26 11:07:34 +0530

    [FEAT]: created home page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav className="border-b p-6">
+        <p className="text-4xl font-bold">Only Fans</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-6 text-blue-500">Home</a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-blue-500">Sell Digital asset</a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-blue-500">My assets</a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-blue-500">Creator Dashboard</a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
