28ba638ecebff0dc42b5b487ac440957c7cfa5bf
ankitpal1029 <ankitkumarpal27@gmail.com>
2021-11-24 21:54:26 +0530

    [FEAT]: temporary route to delete all vouchers in db

--- voucher-backend/src/routes/vouchers.ts
+++ voucher-backend/src/routes/vouchers.ts
@@ -2,10 +2,12 @@ import express from "express";
 import FetchAllVouchers from "../controllers/fetch-all-vouchers";
 import CreateVoucher from "../controllers/create-voucher";
 import FetchCurrentId from "../controllers/fetch-current-id";
+import DeleteAllVouchers from "../controllers/delete-all-vouchers";
 const router = express.Router();
 
 router.get("/fetchVouchers", FetchAllVouchers);
 router.post("/addVoucher", CreateVoucher);
 router.get("/getCurrentId", FetchCurrentId);
+router.post("/deleteAll", DeleteAllVouchers);
 
 export default router;
