592f9b7798ed41ee9f0d399c4582f411e27e7a22
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-02-27 18:19:27 +0530

    access control issue 2

--- frontend-contracts/pages/auth/twitch/callback.js
+++ frontend-contracts/pages/auth/twitch/callback.js
@@ -5,17 +5,20 @@ import { useRouter } from "next/router";
 
 const CallbackAuth = () => {
   const Router = useRouter();
+  const twitchCallback = async (access_token) => {
+    try {
+      await axios.get(`/auth/twitch/callback?access_token=${access_token}`);
+      Router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
     const parsedHash = new URLSearchParams(window.location.hash.substring(1));
     if (parsedHash.get("access_token")) {
-      var access_token = parsedHash.get("access_token");
+      let access_token = parsedHash.get("access_token");
       console.log(access_token);
-      try {
-        await axios.get(`/auth/twitch/callback?access_token=${access_token}`);
-        Router.push("/");
-      } catch (error) {
-        console.log(error);
-      }
+      twitchCallback(access_token);
     }
   });
   return (
