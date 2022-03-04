9f5bdf1519777e99ce61c9a522c623ff7a502aa4
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-03-04 21:09:29 +0530

    change twitch callback

--- frontend-contracts/lib/twitch_redirect.js
+++ frontend-contracts/lib/twitch_redirect.js
@@ -1,6 +1,6 @@
 const twitchRedirect =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000/auth/twitch/callback"
-    : "https://nft-streamer-marketplace.herokuapp.com/auth/twitch/callback";
+    : "https://marketplace.nfstreamer.tech/auth/twitch/callback";
 
 export default twitchRedirect;
