d25f48a233a50527907f07f7dc97f1192084a2df
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-03-04 11:23:12 +0530

    use .tech domain

--- frontend-contracts/lib/axios_config.js
+++ frontend-contracts/lib/axios_config.js
@@ -3,7 +3,7 @@ import Axios from "axios";
 const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://nft-streamer-backend.herokuapp.com";
+    : "https://api.nfstreamer.tech";
 
 const axios = Axios.create({
   baseURL,
