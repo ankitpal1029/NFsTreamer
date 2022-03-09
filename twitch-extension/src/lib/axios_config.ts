a5d6b48e06922123708fe00b7cabf7a45ffdfc8e
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-03-09 11:42:19 +0530

    upgraded twitch-extension

--- twitch-extension/src/lib/axios_config.ts
+++ twitch-extension/src/lib/axios_config.ts
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 export const baseURL =
   process.env.NODE_ENV === "development"
-    ? "https://484e-49-204-132-57.ngrok.io"
+    ? "https://e931-49-205-85-29.ngrok.io"
     : "https://nft-streamer-backend.herokuapp.com";
 
 const axios = Axios.create({
