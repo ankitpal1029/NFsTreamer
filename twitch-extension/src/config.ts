20f72d03e8208ef0f7381276f5cf7ad826d56f23
ankitpal1029 <ankitkumarpal27@gmail.com>
2022-04-12 11:11:58 +0530

    change backend to api.nfstreamer.tech

--- twitch-extension/src/config.ts
+++ twitch-extension/src/config.ts
@@ -2,4 +2,5 @@ export const lazynftaddress =
   process.env.NODE_ENV === "development"
     ? "0x8464135c8F25Da09e49BC8782676a84730C318bC"
     : "0x5B63799C7ba300b483095411861f1327699337A8";
-export const IPFS_GATEWAY = "https://ipfs.infura.io/ipfs/";
+// export const IPFS_GATEWAY = "https://ipfs.infura.io/ipfs/";
+export const IPFS_GATEWAY = "https://ipfs.io/ipfs/";
--- twitch-extension/src/lib/axios_config.ts
+++ twitch-extension/src/lib/axios_config.ts
@@ -3,7 +3,7 @@ import Axios from "axios";
 export const baseURL =
   process.env.NODE_ENV === "development"
     ? "https://224b-2401-4900-2321-5b60-cf68-e4be-d188-1a57.ngrok.io"
-    : "https://nft-streamer-backend.herokuapp.com";
+    : "https://api.nfstreamer.tech";
 
 console.log(baseURL);
 const axios = Axios.create({
